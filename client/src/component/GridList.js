a21b6fff42d467d9991840517cf440be6e73ce48
hwayoung98 <hwa3270@gmail.com>
2020-12-24 02:19:03 +0900

    [chor] update gridlist

--- client/src/component/GridList.js
+++ client/src/component/GridList.js
@@ -1,5 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
+=======
+=======
+import React, { useEffect, useState  } from 'react';
+>>>>>>> 7d594ef ([chor] update gridlist)
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -8,6 +13,11 @@ import Container from '@material-ui/core/Container';
 import Front from './card/Front';
 import Back from './card/Back';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
+<<<<<<< HEAD
+=======
+import CardFrame from './card/CardFrame';
+import axios from "axios";
+>>>>>>> 7d594ef ([chor] update gridlist)
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const apiURL = "http://localhost:8000";
@@ -18,6 +28,7 @@ const GridList = ({category}) => {
   const [datetime, setDatetime] = useState('20201202');
   const [image_url, setImage_url] = useState(null);
   const [title, setTitle] = useState(null);
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 =======
@@ -64,16 +75,61 @@ const GridList = ({category}) => {
         
       })
   })
+=======
+  const [test, setTest] = useState(null);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      setError(null);
+      setData(null);
+      setLoading(true);
+      const response = await axios.get(`${apiURL}/test`, {
+        params : {
+          datetime : datetime,
+          cate : currentCategory,
+        }
+      })
+      setData(response.data);
+    } catch(e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
+
+  useEffect(() => {
+    setCategory(category);
+    fetchData();
+    }, []);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!data) return null;
+
+>>>>>>> 7d594ef ([chor] update gridlist)
   return (
     <React.Fragment>
         <CssBaseline/>
           <main>
             <Typography>{currentCategory}</Typography>
+<<<<<<< HEAD
             <Typography>{test}</Typography>
             <Container className={classes.cardGrid} maxWidth="md">
               <Grid container spacing={4}>
                 {cards.map((card) => (
                   <Grid item key={card} xs={12} sm={6} md={4}>
+=======
+            <Typography>{data.title}</Typography>
+            <Typography>{title}</Typography>
+            <Container className={classes.cardGrid} maxWidth="md">
+              <Grid container spacing={4}>
+                {data.map((doc, card) => (
+                  <Grid item key={card} xs={12} sm={6} md={4}>
+                    <Typography>{card.title}</Typography>
+>>>>>>> 7d594ef ([chor] update gridlist)
                     <Flippy className={classes.card}
                       flipOnHover={false} // default false
                       flipOnClick={true} // default false
@@ -120,5 +176,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default GridList;
+<<<<<<< HEAD
 =======
 >>>>>>> 8d86167 (test for route function)
+=======
+>>>>>>> e305d79 ([chor] update gridlist)
+>>>>>>> 7d594ef ([chor] update gridlist)
