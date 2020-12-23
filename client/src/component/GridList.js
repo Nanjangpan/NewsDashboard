5a1cfb07a4feb8292852b543cbb3caa234bd06f7
hwayoung98 <hwa3270@gmail.com>
2020-12-24 02:30:15 +0900

    [chor] merge gridlist file

--- client/src/component/GridList.js
+++ client/src/component/GridList.js
@@ -1,29 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react';
-=======
-=======
-=======
->>>>>>> a30ac7f (merge branch backends)
 import React, { useEffect, useState  } from 'react';
->>>>>>> 7d594ef ([chor] update gridlist)
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-<<<<<<< HEAD
 import Front from './card/Front';
 import Back from './card/Back';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
-<<<<<<< HEAD
-=======
-=======
->>>>>>> a30ac7f (merge branch backends)
 import CardFrame from './card/CardFrame';
 import axios from "axios";
->>>>>>> 7d594ef ([chor] update gridlist)
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const apiURL = "http://localhost:8000";
@@ -34,73 +19,10 @@ const GridList = ({category}) => {
   const [datetime, setDatetime] = useState('20201202');
   const [image_url, setImage_url] = useState(null);
   const [title, setTitle] = useState(null);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> b1eee6c (feat: add card flip function)
-  const [test, setTest] = useState(null);
-  useEffect(() => {
-    setCategory(category);
-    let items = [];
-    fetch(`${apiURL}/test/go?datetime=${datetime}`)
-      .then(response => response.json())
-      .then((data) => {
-        setTest(data);
-        })
-  })
-<<<<<<< HEAD
->>>>>>> f720f34 ([feat] add Route component)
-=======
-=======
->>>>>>> fdc887c (feat: add card flip function)
->>>>>>> b1eee6c (feat: add card flip function)
-
-  const _getLiveData = ({datetime}) => {
-    let items = [];
-    fetch('${apiURL}/test/go/')
-  }
-
-  useEffect(() => {
-    setCategory(category);
-    let items = [];
-    await fetch("test/go")
-      .then(response => response.json())
-      .then((data) => {
-        data.forEach((doc) => {
-          items.push({
-            image_url : doc.image_url,
-            title : doc.title,
-            url : doc.url,
-            keyword : doc.keyword,
-            press : doc.press,
-            title_list
-          })
-        })
-        
-      })
-  })
-=======
   const [test, setTest] = useState(null);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-
-  // const message = async ()=> {
-  //   try {
-  //     let res = await axios.get('http://localhost:8000/test', {
-  //       params : {
-  //         datetime : datetime,
-  //         cate : currentCategory,
-  //       }
-  //     });
-  //     let result = res.data.message;
-  //     setData(result);
-  //   } catch(e) {
-  //     console.log(e);
-  //   }
-  // };
 
   const fetchData = async () => {
     try {
@@ -127,36 +49,18 @@ const GridList = ({category}) => {
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
   if (!data) return null;
-<<<<<<< HEAD
 
->>>>>>> 7d594ef ([chor] update gridlist)
-=======
->>>>>>> a30ac7f (merge branch backends)
   return (
     <React.Fragment>
         <CssBaseline/>
           <main>
-<<<<<<< HEAD
             <Typography>{currentCategory}</Typography>
-<<<<<<< HEAD
-            <Typography>{test}</Typography>
-            <Container className={classes.cardGrid} maxWidth="md">
-              <Grid container spacing={4}>
-                {cards.map((card) => (
-                  <Grid item key={card} xs={12} sm={6} md={4}>
-=======
-=======
->>>>>>> a30ac7f (merge branch backends)
             <Typography>{data.title}</Typography>
-            <Typography>{currentCategory}</Typography>
             <Typography>{title}</Typography>
             <Container className={classes.cardGrid} maxWidth="md">
-<<<<<<< HEAD
               <Grid container spacing={4}>
                 {data.map((doc, card) => (
                   <Grid item key={card} xs={12} sm={6} md={4}>
-                    <Typography>{card.title}</Typography>
->>>>>>> 7d594ef ([chor] update gridlist)
                     <Flippy className={classes.card}
                       flipOnHover={false} // default false
                       flipOnClick={true} // default false
@@ -176,7 +80,6 @@ const GridList = ({category}) => {
                 ))}
               </Grid>          
             </Container>
-=======
             <Grid container spacing={4}>
               {data.map((card) => (
                 <Grid item xs={12} sm={6} md={4}>
@@ -185,8 +88,6 @@ const GridList = ({category}) => {
                 </Grid>
               ))}
             </Grid>
-          </Container>
->>>>>>> a30ac7f (merge branch backends)
           </main>
     </React.Fragment>
   );  
@@ -213,14 +114,4 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-<<<<<<< HEAD
 export default GridList;
-<<<<<<< HEAD
-=======
->>>>>>> 8d86167 (test for route function)
-=======
->>>>>>> e305d79 ([chor] update gridlist)
->>>>>>> 7d594ef ([chor] update gridlist)
-=======
-export default GridList;
->>>>>>> a30ac7f (merge branch backends)
