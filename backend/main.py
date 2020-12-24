b64b7d7ec6e187944395ba513702b5d6952e840e
hwayoung98 <hwa3270@gmail.com>
2020-12-24 22:53:26 +0900

    [chor] add show key

--- backend/main.py
+++ backend/main.py
@@ -13,10 +13,11 @@ Hot_live_Data = [
     {
         'cluster_num' : 1,
         'image_url': "https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/68928347_372170763455402_1476590304457916416_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=4qrCUJ1JsxAAX-YEYU1&tp=1&oh=bf11422bdd01df68152b248c41aafaf4&oe=600E61FC",
-        'title': "1_'머리 때리고 코 비틀고' 어린이집 원생 7명 학대...20대 교사 입건rrrrrrrrrrrrrr",
+        'title': "1_'머리 때리고 코 비틀고' 어린이집 원생 7명 학대...20대 교사 입건",
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
         'keyword': ["이시은", "바보", "멍청이"],
+        'show' : 'True',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
@@ -55,12 +56,14 @@ Hot_live_Data = [
         'title': "2_'머리 때리고 코 비틀고' 어린이집 원생 7명 학대...20대 교사 입건",
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
-        'keyword': ["이시은", "바보", "멍청이"],
+        'keyword': ["이시은", "바보", "멍청이"],        
+        'show' : 'True',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
                 'url' : "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=101&oid=001&aid=0012098013",
-                'press' : "YBIGTA"
+                'press' : "YBIGTA",
+                'show' : 'True'
             },{
                 'title' : "잠시 뒤 정경심 1심 선고...법원 결론은?",
                 'url' : "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=052&aid=0001529633",
@@ -95,6 +98,7 @@ Hot_live_Data = [
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
         'keyword': ["이시은", "바보", "멍청이"],
+        'show' : 'True',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
@@ -134,6 +138,7 @@ Hot_live_Data = [
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
         'keyword': ["이시은", "바보", "멍청이"],
+        'show' : 'True',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
@@ -173,6 +178,7 @@ Hot_live_Data = [
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
         'keyword': ["이시은", "바보", "멍청이"],
+        'show' : 'True',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
@@ -212,6 +218,7 @@ Hot_live_Data = [
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
         'keyword': ["이시은", "바보", "멍청이"],
+        'show' : 'True',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
@@ -251,6 +258,7 @@ Hot_live_Data = [
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
         'keyword': ["이시은", "바보", "멍청이"],
+        'show' : 'True',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
@@ -290,6 +298,7 @@ Hot_live_Data = [
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
         'keyword': ["이시은", "바보", "멍청이"],
+        'show' : 'True',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
@@ -329,6 +338,7 @@ Hot_live_Data = [
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
         'keyword': ["이시은", "바보", "멍청이"],
+        'show' : 'True',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
@@ -368,6 +378,7 @@ Hot_live_Data = [
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
         'keyword': ["이시은", "바보", "멍청이"],
+        'show' : 'wait',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
@@ -407,6 +418,7 @@ Hot_live_Data = [
         'url': "https://news.naver.com/main/ranking/read.nhn?mode=LSD&mid=shm&sid1=001&oid=052&aid=0001529635&rankingType=RANKING",
         'press': "YTN",
         'keyword': ["이시은", "바보", "멍청이"],
+        'show' : 'wait',
         'title_list': [
             {
                 'title' : "명동 네이`처리퍼블릭 공시가격 3.8% 오르는데 보유세는 26.8%↑(종합)",
